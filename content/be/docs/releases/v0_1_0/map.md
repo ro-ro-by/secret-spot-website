c878c409e2a418a497b594b84aaf4556714837f6
Max Stsepantsevich <maxim.stsepantsevich@gmail.com>
2024-03-14 23:50:59 +0200

    Add map demo

---
+++ content/be/docs/releases/v0_1_0/map.md
@@ -0,0 +1,6 @@
+---
+title: 'map'
+---
+# release v0.1.0 / map
+
+{{< map height="800" >}} 
---
+++ static/js/map.js
@@ -0,0 +1,132 @@
+(function (window, ol) {
+
+    /**
+     * Add event listener on feature click.
+     */
+    function onFeatureClick(map, layer, callback) {
+        map.on('click', (e) => {
+            layer.getFeatures(e.pixel)
+                .then(function (features) {
+                    const feature = features[0] ?? null;
+
+                    if (!feature) {
+                        return;
+                    }
+
+                    callback(feature);
+                });
+        });
+    }
+
+    function cursorPointerOnFeatureHover(map) {
+        map.on('pointermove', (e) => {
+            const mapContainer = map.getTargetElement();
+            const pixel = map.getEventPixel(e.originalEvent);
+            const hit = map.hasFeatureAtPixel(pixel);
+
+            mapContainer.style.cursor =  hit ? 'pointer' : '';
+        });
+    }
+
+    /**
+     * Build map to container.
+     *
+     * @param container
+     * @returns {ol.Map}
+     */
+    function buildMap(container) {
+        const osmLayer = new ol.layer.Tile({
+            className: 'bw',
+            source: new ol.source.OSM(),
+        });
+
+        const rasterLayer = new ol.layer.Tile({
+            source: new ol.source.XYZ({
+                attributions: '© SecretSpot',
+                url: 'https://tiles.secret-spot-by.com/raster/{z}/{x}/{y}.png',
+                maxZoom: 11,
+            }),
+        });
+
+        const vectorLayer = new ol.layer.VectorTile({
+            declutter: true,
+            source: new ol.source.VectorTile({
+                attributions: '© SecretSpot',
+                format: new ol.format.MVT(),
+                url: 'https://tiles.secret-spot-by.com/vector/{z}/{x}/{y}.pbf',
+                maxZoom: 9,
+            }),
+            style: function (feature) {
+                const title = feature.get('title');
+                const type = feature.get('type');
+
+                let color = 'gray';
+                switch (type) {
+                    case 'spring':
+                        color = 'blue';
+                        break;
+                    case 'tree':
+                        color = 'orange';
+                        break;
+                    case 'boulder':
+                        color = 'red';
+                        break;
+                }
+
+                return new ol.style.Style({
+                    image:  new ol.style.Circle({
+                        radius: 5,
+                        fill: new ol.style.Fill({color: color}),
+                        stroke: new ol.style.Stroke({color: 'black', width: 2}),
+                    }),
+                    text: new ol.style.Text({
+                        text: title,
+                        fill: new ol.style.Fill({color: 'black'}),
+                        stroke: new ol.style.Stroke({color: 'gray', width: 1}),
+                        scale: 1.2,
+                        textAlign: 'left',
+                        offsetX: 10,
+                        offsetY: -20
+                    }),
+                });
+            },
+        });
+
+        const map = new ol.Map({
+            controls: ol.control.defaults.defaults().extend([new ol.control.FullScreen()]),
+            layers: [
+                osmLayer,
+                rasterLayer,
+                vectorLayer,
+            ],
+            target: container,
+            view: new ol.View({
+                center: ol.proj.fromLonLat([27.95, 53.70]),
+                zoom: 6,
+            }),
+        });
+
+        const onResolutionChange = (map) => {
+            const isLowResolution = map.getView().getResolution() < 50;
+
+            rasterLayer.setVisible(!isLowResolution);
+            vectorLayer.setVisible(isLowResolution);
+        };
+
+        map.getView().on('change:resolution', () => onResolutionChange(map));
+        onFeatureClick(map, vectorLayer, (feature) => {
+            const url = `/be/docs/releases/v0_1_0/data/#item-${feature.get('uid')}`
+
+            window.location.href = url;
+        });
+        onResolutionChange(map);
+        cursorPointerOnFeatureHover(map);
+
+
+        return map;
+    }
+
+    window.SecretSpotMap = {
+        buildMap,
+    };
+})(window, ol);
